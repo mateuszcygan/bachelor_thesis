2a1b8a74e7256fbd0e36bd1a572f036e69e5b6c7
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-02-16 10:11:05 +0100

    random rewards and probabilities for MDP implemented

--- MDP.py
+++ MDP.py
@@ -31,27 +31,19 @@ transitions = {}
 
 for state in states:
     transition = create_transitions(state, actions, transition_keys)
+    total_prob = 0
+    # Generate a random following state and a random reward
     for action in actions:
         transition[state][action]['foll_state'] = random.choice(states)
+        transition[state][action]['reward'] = random.randint(-10, 10)
+    for action in actions[:-1]:  # Exclude the last action, as it will be adjusted later
+        # Generate a random probability with two decimal places
+        prob = round(random.uniform(0, 1 - total_prob), 2)
+        total_prob += prob
+        transition[state][action]['probability'] = prob
+    # Adjust the probability of the last action to ensure the sum is exactly 1.0
+    prob_c = round(1 - total_prob, 2)
+    transition[state]['c']['probability'] = prob_c
     print_transition_details(transition)
-
-
-
-
-#     possible_actions = random.sample(actions, k=random.randint(1, len(actions))) #randomly select actions
-#     #randint(start, end)
-#     #returns random integer in range [start, end], end points included
-#     #random.sample(sequence, k)
-#     #returns k length new list of elements chosen from the sequence
-
-#     #print(possible_actions)
-#     for poss_action in possible_actions:
-#         transition = create_dictionary(state, transition_keys)
-#         transition[state]['action'] = poss_action
-#         transition[state]['foll_state'] = random.choice(states)
-#         transition[state]['reward'] = random.randint(0, 10)
-#         transitions.append(transition)
-
-# print(*transitions, sep="\n")
     
 
