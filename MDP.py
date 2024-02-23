15daac89f355f0617c23889c1960e1f0e0de22c4
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-02-23 14:52:28 +0100

    function that returns a list with actions that can be executed from a certain state (based on list P - list with probabilities)

--- MDP.py
+++ MDP.py
@@ -59,10 +59,16 @@ def get_foll_states_rewards_values(R, executed_action, current_state):
 def get_foll_states_prob_values(P, current_state, executed_action):
     return [P[current_state][executed_action][prob] for prob in P[current_state][executed_action]]
 
-# Return an array with possibe following states from current state (based on Rewards set)
+# Return an array with possibe following states from certain state (based on Rewards set)
 # Create a set that to each state assign following states - use this in this function to return following states (upcoming change)
 def get_following_states(R, current_action, current_state):
     return list(R[current_action][current_state].keys())
+
+# Return an array with possible actions that can be executed from a certain state
+def get_possible_actions(P, state):
+    return list(P[state].keys())
+
+
 
 
 def createMDP():
