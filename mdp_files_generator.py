e5ddfb0975fc768f12d9c332702c0a9e07a8253b
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-02-26 10:21:54 +0100

    Function read_saved_mdp returns an mdp object instead of mdp properties

--- mdp_files_generator.py
+++ mdp_files_generator.py
@@ -9,15 +9,10 @@ def save_object(obj, filename):
     with open(filename, 'wb') as outp:  # Overwrites any existing file.
         pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
 
-def saved_mdp_prop(filename):
+def read_saved_mdp(filename):
     with open(filename, 'rb') as inp:
           mdp = pickle.load(inp)
-          S = mdp.states
-          A = mdp.actions
-          R = mdp.rewards
-          P = mdp.probabilities
-          return S, A, R, P
-
+          return mdp
 
 
 
