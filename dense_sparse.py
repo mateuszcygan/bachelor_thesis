3ebc4296da62198f0ad3414ad25659f1d7f7ac38
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-10 11:55:27 +0100

    function that thins possible following states completely implemented

--- dense_sparse.py
+++ dense_sparse.py
@@ -2,7 +2,7 @@ import mdp
 import random
 import mdp_files_generator as mfg
 import print_mdp
-import math_functions as math
+import math_functions as math_fct
 
 # Function that outputs how many states have the probability 0.0 (are unreachable)
 def unreachable_states(mdp_object):
@@ -25,60 +25,34 @@ def unreachable_states(mdp_object):
 
     print("Unreachable states in the whole MDP:", unreachable_overall)
 
-# test_mdp = mdp.createMDPupdated()
-# unreachable_states(test_mdp)
-
-# sparse mdps: take already created mdp, go through probabilities, change values some of them to 0
-# P = {
-#       's0' : {'a0' : { 's0' : 0.75, 's1' : 0.2, 's2' : 0.05},
-#               'a1' : { 's0' : 0.3, 's1' : 0.2, 's2' : 0.5}
-#               'a2' : { 's0' : 0.9, 's1' : 0.05, 's2' : 0.05}
-#       }
-#       's1' : {'a0' : { 's0' : 0.0, 's1' : 0.2, 's2' : 0.8},
-#              { 'a1' : { ... }}
-#       }
-#     }
-
-def sparse_prob(mdp, state_sparsity_rate):
+# Thinning the reachable states in the MDP by using state_sparsity_rate - percentage of states that should be unreached
+def sparse_mdp_states(mdp, state_sparsity_rate):
 
     S = mdp.states
     A = mdp.actions
     P = mdp.probabilities
+    
     old_probability_weight = 1 - state_sparsity_rate
-    sparsity_weights = [state_sparsity_rate, old_probability_weight] # Sparse weights for probabilities 0.0 and going over to another state
+    sparsity_weights = [state_sparsity_rate, old_probability_weight] # Sparse weights for 0.0 probability and probability of going over to following state
 
     for current_state in S:
         for action in A:
             for following_state in S:
-                possible_prob_values = [0.0] # Array to choose between 0.0 and probability value of going over to another state
+
+                possible_prob_values = [0.0]  # Array containing 0.0 probability and probability of transitioning to following state (used for decision-making)
                 prob_value = P[current_state][action][following_state]
 
-                # If any of the states is already unreachable, don't change it
+                # If any of the following states is already unreachable, don't change it
                 if prob_value == 0:
                     continue
 
                 possible_prob_values.append(prob_value)
+
+                # Choose between 0.0 and old probability of transitioning to following state
                 new_prob_value = random.choices(possible_prob_values, weights = sparsity_weights)[0]
                 P[current_state][action][following_state] = new_prob_value
-            
-            action_prob = list(P[current_state][action].values())
-            print("New probability values for action", action, action_prob)
-            math.normalize(action_prob)
-            print("New probability values for action after normalization", action, action_prob)
-
-        print("\n")
-
-mdp_test = mfg.read_saved_mdp("mdp_object.pkl")
-# print("States:", mdp_test.states)
-# print("Actions:", mdp_test.actions)
-print("Probabilities:")
-print_mdp.print_prob_details(mdp_test.probabilities)
-# print("Rewards:", mdp_test.rewards)
-
-sparse_prob(mdp_test, 0.5)
-print(mdp_test.probabilities)
-
-# print(mdp_test.probabilities)
+    
+    math_fct.normalize_mdp_probabilities(mdp)
 
     
 
