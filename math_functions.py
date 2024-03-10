ee59cf0fbc0ef5a33f563432225385a91d3947e0
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-10 11:36:25 +0100

    function that directly normalizes mdp probabilities implemented

--- math_functions.py
+++ math_functions.py
@@ -1,7 +1,9 @@
-# Normalization (probability) 
-
 def normalize_and_round(probabilities):
     total = sum(probabilities)
+    
+    if total == 0:
+        return
+
     normalized_probabilities = [prob / total for prob in probabilities]
     rounded_probabilities = [round(prob, 2) for prob in normalized_probabilities]
 
@@ -18,7 +20,12 @@ def normalize_and_round(probabilities):
             
 def normalize(probabilities):
     total = sum(probabilities)
+
+    if total == 0:
+        return
+    
     normalized_probabilities = [prob / total for prob in probabilities]
+    print(normalized_probabilities)
     return normalized_probabilities
 
 # Example to ask about
@@ -27,4 +34,24 @@ test_arr = normalize(test)
 print(test_arr)
 print(sum(test_arr))
 
+# Normalize probabilities for following states
+def normalize_mdp_probabilities(mdp):
 
+    S = mdp.states
+    A = mdp.actions
+    P = mdp.probabilities
+
+    for current_state in S:
+        for action in A:
+
+            # Calculate the total value of probabilities for certain action
+            action_prob = list(P[current_state][action].values())
+            total = sum(action_prob)
+
+            for foll_state in S:
+
+                # Normalize the probability of following states
+                try:
+                    P[current_state][action][foll_state] = P[current_state][action][foll_state]/total
+                except ZeroDivisionError:
+                    P[current_state][action][foll_state] = 0
