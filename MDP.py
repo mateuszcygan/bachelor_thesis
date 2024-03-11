3b563b6320135c876eb9049f0d8674e15e93ca53
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-11 20:22:31 +0100

    Function that returns following states derived from Rewards' set deleted

--- MDP.py
+++ MDP.py
@@ -33,11 +33,6 @@ def get_foll_states_rewards_values(R, current_state, executed_action):
 def get_foll_states_prob_values(P, current_state, executed_action):
     return list(P[current_state][executed_action].values())
 
-# Return an array with possibe following states from certain state (based on Rewards set)
-# Create a set that to each state assign following states - use this in this function to return following states /upcoming change
-def get_following_states(R, current_action, current_state):
-    return list(R[current_action][current_state].keys())
-
 # Return an array with possible actions that can be executed from a certain state
 def get_possible_actions(P, state):
     return list(P[state].keys())
