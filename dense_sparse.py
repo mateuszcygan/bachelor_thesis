610948590735491e09f4babfccfd9123dc7cf826
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-11 17:37:01 +0100

    Calculation of unreachble states is added

--- dense_sparse.py
+++ dense_sparse.py
@@ -4,12 +4,14 @@ import mdp_files_generator as mfg
 import print_mdp
 import math_functions as math_fct
 
-# Function that outputs how many states have the probability 0.0 (are unreachable)
+# Function that outputs the number of states with a probability of 0.0 (indicating unreachable states)
 def unreachable_states(mdp_object):
+
     states = mdp_object.states
     actions = mdp_object.actions
     P = mdp_object.probabilities
-    print("Number of possible states:", len(states))
+
+    poss_states_transitions = len(states)*len(states)*len(actions)
 
     unreachable_overall = 0
 
@@ -23,7 +25,25 @@ def unreachable_states(mdp_object):
             print("Unreachable states:", unreachable)
             unreachable_overall += unreachable
 
+    print("Number of transitions to other states:", poss_states_transitions)
     print("Unreachable states in the whole MDP:", unreachable_overall)
+    
+    unreachable_percentage = unreachable_overall/poss_states_transitions
+    print("Percentage of unreachable states:", unreachable_percentage)
+
+# For states without following actions (all probabilities are set to zero) randomly choose one following state
+def random_following_state(mdp):
+    print("\nrandom following state function")
+
+    S = mdp.states
+    A = mdp.actions
+    P = mdp.probabilities
+
+    for current_state in S:
+        for action in A:
+            print(list(P[current_state][action].values()))
+
+
 
 # Thinning the reachable states in the MDP by using state_sparsity_rate - percentage of states that should be unreached
 def sparse_mdp_states(mdp, state_sparsity_rate):
@@ -51,8 +71,11 @@ def sparse_mdp_states(mdp, state_sparsity_rate):
                 # Choose between 0.0 and old probability of transitioning to following state
                 new_prob_value = random.choices(possible_prob_values, weights = sparsity_weights)[0]
                 P[current_state][action][following_state] = new_prob_value
-    
+
     math_fct.normalize_mdp_probabilities(mdp)
+    random_following_state(mdp)
+
+
 
     
 
