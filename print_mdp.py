fc389913f94d422574d38c64a6988a85292789bf
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-11 20:29:31 +0100

    Renaming function that prints R and P sets in straightforward form

--- print_mdp.py
+++ print_mdp.py
@@ -1,9 +1,4 @@
-def print_rewards_details(rewards_dict):
-    for state, actions_dict in rewards_dict.items():
-        print(state, ":", actions_dict)
-    print('\n')
-
-def print_prob_details(prob_dict):
+def print_mdp_details(prob_dict):
     for state, action_prob_dict in prob_dict.items():
         print(state)
         for action, prob in action_prob_dict.items():
