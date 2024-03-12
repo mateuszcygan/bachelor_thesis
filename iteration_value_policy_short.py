ad8483f1afb073bc3ef69f05b633054b813121a7
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-12 22:31:41 +0100

    policy dictionary implemented

--- iteration_value_policy_short.py
+++ iteration_value_policy_short.py
@@ -1,65 +1,56 @@
-import mdp
-import print_mdp as p
-import mdp_files_generator as mfg
-import iteration_value_policy as ivp
-
-def print_value_function(V):
-    for state, value in V.items():
-        print(state, ":", value)
-
+import print_mdp 
 
 def value_iteration(mdp_object):
 
     # Extract MDP properties
-    S, A, R, P = mdp_object.get_properties() # Extract all properties of MDP
-    print("States: ", S)
-    print("Actions: ", A)
-    print("Rewards: ")
-    p.print_prob_details(R)
-    print("Probabilities: ")
-    p.print_prob_details(P)
+    S, A, R, P = mdp_object.get_properties()
 
     dicount_factor = 0.9
 
     # Initialize the value for each state to 0
     V = {s : 0 for s in S}
     V_new = {s : 0 for s in S}
+    policy = {s : None for s in S} # Dictionary that represents calculated policy
 
-    i = 0
+    i = 0 # Number of iterations
+
     while True:
+
         for current_state in S:
 
-            values = []
+            actions_values = {action : 0 for action in A} # Dictionary with value for each action is a specific state
+
             for a in A:
+
                 # Get probabilities and rewards for current state
                 probabilities = list(P[current_state][a].values())
-                rewards = list(R[a][current_state].values())
+                rewards = list(R[current_state][a].values())
 
                 # Calculate sum(Pa(s,s')(Ra(s,s') + dicount_factor*V_i(s')))
                 value = sum([prob * (reward + dicount_factor * V[current_state]) for prob, reward in zip(probabilities, rewards)])
-                values.append(value)
+                actions_values[a] = value
             
-            print("Calculated values for state", current_state, ":", values)
-            V_new[current_state] = max(values) # Set maximum of calculated values as a new value for current_state
+            print("Calculated values for state", current_state, ":", actions_values)
+
+            # Find the biggest value and its action
+            max_value = max(list(actions_values.values()))
+            biggest_value_action = list(actions_values.keys())[list(actions_values.values()).index(max_value)]
+
+            policy[current_state] = biggest_value_action # Set the action with the biggest value as the policy action from current state
+
+            V_new[current_state] = max_value # Set maximum of calculated values as a new value for current_state
+            
             i += 1
             print("Iteration number", i, "\n")
 
             # Termination condition
             print("Old value function:")
-            print_value_function(V)
+            print_mdp.print_value_function(V)
             print('\n')
             print("New value function:")
-            print_value_function(V_new)
+            print_mdp.print_value_function(V_new)
             print("\n")
             if (all( abs(V[s] - V_new[s]) < 0.0001 for s in S)):
                 print("Number of iterations: ", i)
-                return V_new
+                return V_new, policy
             V = V_new.copy()
-
-# mdp_object = mdp.createMDPupdated()
-# value_iteration(mdp_object)
-
-mdp_updated = mfg.read_saved_mdp("mdp_object.pkl")
-
-y = value_iteration(mdp_updated)
-print("Result:", y)
