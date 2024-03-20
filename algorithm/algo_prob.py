9fedcf5ee82cb4c58950392f0b678be65269fb22
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-20 11:26:25 +0100

    iteration_num - new parameter, determines how many aciton executions should take place

--- algorithm/algo_prob.py
+++ algorithm/algo_prob.py
@@ -1,10 +1,8 @@
 import random
 import copy
 from mdp_obj import mdp
-from mdp_obj import print_mdp
 
-
-def learn_probabilities(mdp_object):
+def learn_probabilities(mdp_object, iteration_num):
 
     # The structure of MDP is known
     A = mdp_object.actions
@@ -21,12 +19,6 @@ def learn_probabilities(mdp_object):
     for state in S:
         state_actions[state]["actions_num"] = len(list(P[state].keys()))
         state_actions[state]["actions"] = list(P[state].keys())
-    
-    # for state in S:
-    #     print(state)
-    #     print("actions_num:", state_actions[state]["actions_num"])
-    #     print("actions:", state_actions[state]["actions"])
-    #     print("\n")
         
     # Store how many times a transition to a certain state took place
     states_hits = {s : { a : {s : 0 for s in S} for a in A } for s in S}
@@ -44,8 +36,11 @@ def learn_probabilities(mdp_object):
                 approximated_prob[state][action][next_state] = initial_prob
 
     current_state = 's0' # Start at state 's0'
+    i = 0 # Number of iterations
 
-    for _ in range(100000):
+    for _ in range(iteration_num):
+        i += 1 # For testing cases
+
         # Retreive which action should be executed as a next one
         action_to_execute_index = state_actions[current_state]["iteration_num"] % state_actions[current_state]["actions_num"]
         executed_action = state_actions[current_state]["actions"][action_to_execute_index]
@@ -68,10 +63,7 @@ def learn_probabilities(mdp_object):
     
         current_state = next_state
 
-    print_mdp.print_mdp_details(approximated_prob)
     return approximated_prob
-
-
     
     
 
