1b7f4c114a4af4def1faa95c285f9909fd9b0b2c
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-21 17:13:02 +0100

    test_prob_sum_convergence - test if sum of approximated probabilities calculated with convergence is equal (close to) 1.0

--- algorithm/test_probabilities.py
+++ algorithm/test_probabilities.py
@@ -5,7 +5,7 @@ from context import mdp
 class TestLearnProbabilities(unittest.TestCase):
 
     # Test if sum of approximated probabilities for each state is equals (close to) 1.0
-    def test_prob_sum(self):
+    def test_prob_sum_finite(self):
 
         for _ in range(100): # Test for 100 different MDPs
 
@@ -15,7 +15,21 @@ class TestLearnProbabilities(unittest.TestCase):
             result = probabilities.learn_probabilities_finite(mdp_object, iteration_number)
 
             # Check if the sum of all probabilities is close to 1
+            for state, action_foll_states in result.items():
+                for action, foll_states in action_foll_states.items():
+                    state_prob_sum = sum(list(foll_states.values()))
+                    self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
+    
+    def test_prob_sum_convergence(self):
 
+        for _ in range(100): # Test for 100 different MDPs
+
+            mdp_object = mdp.createMDP()
+            threshold = 0.001
+
+            result = probabilities.learn_probabilities_convergence(mdp_object, threshold)
+
+            # Check if the sum of all probabilities is close to 1
             for state, action_foll_states in result.items():
                 for action, foll_states in action_foll_states.items():
                     state_prob_sum = sum(list(foll_states.values()))
